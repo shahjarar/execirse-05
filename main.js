5df7be5df675feedb8493a6388cb9ed81c91a438
shahjarar <160387866+shahjarar@users.noreply.github.com>
2024-02-29 13:55:42 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,3 @@
+var famousPerson = "Albert Einstein";
+var message = "once said, A person who never made a mistake never learn any think new";
+console.log(famousPerson, message);
---
+++ main.ts
@@ -0,0 +1,5 @@
+let famousPerson = "Albert Einstein";
+
+let message = "once said, A person who never made a mistake never learn any think new";
+
+console .log(famousPerson,message);
